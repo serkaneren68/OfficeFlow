9911b8de1120fee3e4ead840d611a3b3926f06c8
Serkan Eren <serkaneren68@gmail.com>
2026-02-20 03:34:14 +0300

    Initial Commit

---
+++ OfcHoursApp/OfcHoursAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfcHoursAppApp.swift
+//  OfcHoursApp
+//
+//  Created by Serkan EREN on 20.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct OfcHoursAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
